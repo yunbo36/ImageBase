0442f45a3f078ad95cbb1b2ddf67215256769c55
yunbo36 <sic.miura@gmail.com>
2024-09-04 04:28:07 +0900

    Add GitHub Actions workflow for Electron build

--- index.js
+++ index.js
@@ -1,30 +1,37 @@
 const { app, BrowserWindow, ipcMain } = require('electron');
 const path = require('path');
+const sqlite3 = require('sqlite3').verbose();
 
 function createWindow() {
-    const win = new BrowserWindow({
+    const preloadPath = path.join(__dirname, 'preload.js');
+    console.log(`Preload script path: ${preloadPath}`);
+
+    const mainWindow = new BrowserWindow({
         width: 1920,
         height: 1080,
         webPreferences: {
-            enableRemoteModule: true,
-            nodeIntegration: true,
+            preload: preloadPath,
             contextIsolation: false,
-            preload: path.join(__dirname, 'preload.js')
+            enableRemoteModule: true,
+            nodeIntegration: true
         }
     });
 
-    win.setMenuBarVisibility(false);
-
-    win.loadFile('index.html');
-    // win.webContents.openDevTools();
-    return win.id;
+    mainWindow.loadFile('index.html');
 }
 
-app.whenReady().then(createWindow);
+app.whenReady().then(() => {
+    createWindow();
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow();
+        }
+    });
+});
 
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') {
         app.quit();
     }
 });
-
